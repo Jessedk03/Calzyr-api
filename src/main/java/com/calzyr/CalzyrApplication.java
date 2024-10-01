94afb1240330d55c29026355dd341175e0237339
Jesse de Koe <jdekoe@logictrade.nl>
2024-10-01 08:54:43 +0200

    My initial commit 👋

---
+++ src/main/java/com/calzyr/CalzyrApplication.java
@@ -0,0 +1,13 @@
+package com.calzyr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalzyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalzyrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calzyr/TestCalzyrApplication.java
@@ -0,0 +1,11 @@
+package com.calzyr;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCalzyrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CalzyrApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
