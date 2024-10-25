35aaa2fbf6ef8fbeee33d166961bb8bdea7e575a
Jesse de Koe <jdekoe@logictrade.nl>
2024-10-25 16:38:33 +0200

    get events now getting specific columns and not the whole table for performance.

--- src/main/java/com/calzyr/repositories/EventsRepository.java
+++ src/main/java/com/calzyr/repositories/EventsRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface EventsRepository extends CrudRepository<EventsModel, Integer> {
 
-    @Query("SELECT e FROM EventsModel e WHERE e.DeletedAt IS NULL")
+    @Query("SELECT e.Title, e.Description, e.StartTime, e.EndTime, e.Location, e.AllDay FROM EventsModel e WHERE e.DeletedAt IS NULL")
     Iterable<EventsModel> findAllEvents();
 }
