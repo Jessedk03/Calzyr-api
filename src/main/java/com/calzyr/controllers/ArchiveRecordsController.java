05bb0b9cded151f96f4e3afe5615c60911c8dfc3
Jesse de Koe <jdekoe@logictrade.nl>
2024-11-13 16:16:54 +0100

    ArchiveRecordsController filled with standard routes

--- src/main/java/com/calzyr/controllers/ArchiveRecordsController.java
+++ src/main/java/com/calzyr/controllers/ArchiveRecordsController.java
@@ -1,4 +1,23 @@
 package com.calzyr.controllers;
 
+import com.calzyr.dto.ArchiveRecordsModel;
+import com.calzyr.repositories.ArchiveRecordsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/record-archive")
 public class ArchiveRecordsController {
+
+    @Autowired
+    private ArchiveRecordsRepository archiveRecordsRepository;
+
+    @GetMapping
+    public Iterable<ArchiveRecordsModel> getAllArchivedRecords() { return archiveRecordsRepository.findAllEvents(); }
+
+    @GetMapping("/{record_type}")
+    public Iterable<ArchiveRecordsModel> getArchivedRecordsByRecordType(@RequestBody String record_type) { return archiveRecordsRepository.findByRecordType(record_type); }
 }
