16e0c335861a35ca8a11cdaa9feb6b67a2c1361e
Jesse de Koe <jdekoe@logictrade.nl>
2024-11-13 16:17:18 +0100

    ArchiveRecordsRepository added a custom query for the record_type

--- src/main/java/com/calzyr/repositories/ArchiveRecordsRepository.java
+++ src/main/java/com/calzyr/repositories/ArchiveRecordsRepository.java
@@ -3,9 +3,13 @@ package com.calzyr.repositories;
 import com.calzyr.dto.ArchiveRecordsModel;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ArchiveRecordsRepository extends CrudRepository<ArchiveRecordsModel, Integer> {
 
     @Query("SELECT ar FROM ArchiveRecordsModel ar")
     Iterable<ArchiveRecordsModel> findAllEvents();
+
+    @Query("SELECT ar FROM ArchiveRecordsModel ar WHERE ar.RecordType = :#{record_type}")
+    Iterable<ArchiveRecordsModel> findByRecordType(@Param("record_type") String record_type);
 }
