9716bf1de8c8778ca31b647a9ca1e42327992635
Jesse de Koe <jdekoe@logictrade.nl>
2024-11-25 10:22:56 +0100

    UserResponseDTO added for a return response

--- src/main/java/com/calzyr/dto/user/UserResponseDTO.java
+++ src/main/java/com/calzyr/dto/user/UserResponseDTO.java
@@ -1,4 +1,28 @@
 package com.calzyr.dto.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
 public class UserResponseDTO {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    public UserResponseDTO(UserDTO user) {
+        this.id = user.getId();
+        this.userName = user.getUsername();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+        this.deletedAt = user.getDeletedAt();
+    }
 }
