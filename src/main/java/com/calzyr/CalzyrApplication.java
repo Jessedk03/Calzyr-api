e50817d716c6446ecb16a9caef78c17300f3b820
Jesse de Koe <jdekoe@logictrade.nl>
2025-01-25 17:47:57 +0100

    added the env config in the main file, so it is calling first thing

--- src/main/java/com/calzyr/CalzyrApplication.java
+++ src/main/java/com/calzyr/CalzyrApplication.java
@@ -1,5 +1,6 @@
 package com.calzyr;
 
+import com.calzyr.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CalzyrApplication {
 
 	public static void main(String[] args) {
+
+		EnvConfig.loadenv();
+
 		SpringApplication.run(CalzyrApplication.class, args);
 	}
 
