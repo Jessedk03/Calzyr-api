97b2ce9dcb8571d9a1343b54cffb59142cd43e99
Jesse <Jessedekoe03@gmail.com>
2025-04-01 16:03:19 +0200

    Added @Repository

--- src/main/java/com/calzyr/repositories/EventRepository.java
+++ src/main/java/com/calzyr/repositories/EventRepository.java
@@ -1,12 +1,14 @@
 package com.calzyr.repositories;
 
-import com.calzyr.dto.events.EventDTO;
+import com.calzyr.dto.event.EventDTO;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface EventRepository extends CrudRepository<EventDTO, Integer> {
 
     @Query("SELECT e.Title, e.Description, e.StartTime, e.EndTime, e.Location, e.AllDay FROM EventDTO e WHERE e.DeletedAt IS NULL")
