9cdd46d766580f40a19f2d105f35ef7557bbbb2e
Jesse <Jessedekoe03@gmail.com>
2025-04-01 16:05:27 +0200

    removed test containers

--- src/test/java/com/calzyr/TestCalzyrApplication.java
+++ src/test/java/com/calzyr/TestCalzyrApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestCalzyrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(CalzyrApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(CalzyrApplication::main).run(args);
 	}
 
 }
