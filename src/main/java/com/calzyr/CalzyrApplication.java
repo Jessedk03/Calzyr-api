459ab7728d18836f3fac19aebd678efa2c0cccbd
Jesse <Jessedekoe03@gmail.com>
2025-04-01 16:06:18 +0200

    @EnableJpa Repositories added because of some lombok errors

--- src/main/java/com/calzyr/CalzyrApplication.java
+++ src/main/java/com/calzyr/CalzyrApplication.java
@@ -3,10 +3,12 @@ package com.calzyr;
 import com.calzyr.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.calzyr.repositories")
 public class CalzyrApplication {
 
 	public static void main(String[] args) {
