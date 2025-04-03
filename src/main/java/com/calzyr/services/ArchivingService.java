f7899b1842436b0a080fab56ae302f04f99a25ed
Jesse <Jessedekoe03@gmail.com>
2025-04-03 17:07:59 +0200

    changed the way you can add an error log. now it saves it in the logs folder and then in their sub folder. that way we manage the logs folder structure.

--- src/main/java/com/calzyr/services/ArchivingService.java
+++ src/main/java/com/calzyr/services/ArchivingService.java
@@ -65,10 +65,10 @@ public class ArchivingService {
                 archiveRecordRepository.save(archiveRecord);
                 userRepository.delete(user);
 
-                loggingService.archiveLog("[" + Timestamp.from(Instant.now()) + "]" + " Archiving user: " + user.getEmail() + " successful");
+                loggingService.saveLog("archive_logs", "[" + Timestamp.from(Instant.now()) + "]" + " Archiving user: " + user.getEmail() + " successful");
             } catch (Exception e) {
                 try {
-                    loggingService.archiveLog("[" + Timestamp.from(Instant.now()) + "]" + " Archiving old users failed: " + e.getMessage() + " Cause: " + e.getCause());
+                    loggingService.saveLog("archive_logs","[" + Timestamp.from(Instant.now()) + "]" + " Archiving old users failed: " + e.getMessage() + " Cause: " + e.getCause());
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -98,10 +98,10 @@ public class ArchivingService {
                 archiveRecordRepository.save(archiveRecord);
                 eventRepository.delete(event);
 
-                loggingService.archiveLog("[" + Timestamp.from(Instant.now()) + "]" + " Archiving event: " + event.getTitle() + " successful");
+                loggingService.saveLog("archive_logs","[" + Timestamp.from(Instant.now()) + "]" + " Archiving event: " + event.getTitle() + " successful");
             } catch (Exception e) {
                 try {
-                    loggingService.archiveLog("[" + Timestamp.from(Instant.now()) + "]" + " Archiving old events failed: " + e.getMessage() + " Cause: " + e.getCause());
+                    loggingService.saveLog("archive_logs","[" + Timestamp.from(Instant.now()) + "]" + " Archiving old events failed: " + e.getMessage() + " Cause: " + e.getCause());
                 } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
