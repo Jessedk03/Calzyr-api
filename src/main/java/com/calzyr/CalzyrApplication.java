07577d523efa929b3fa32f4fdda1bedda1e0586b
Jesse <Jessedekoe03@gmail.com>
2025-04-03 17:08:48 +0200

    .env file added. now the enviroment variables are now being loaded in.

--- src/main/java/com/calzyr/CalzyrApplication.java
+++ src/main/java/com/calzyr/CalzyrApplication.java
@@ -1,6 +1,7 @@
 package com.calzyr;
 
 import com.calzyr.config.EnvConfig;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -14,6 +15,11 @@ public class CalzyrApplication {
 	public static void main(String[] args) {
 
 		EnvConfig.loadenv();
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 
 		SpringApplication.run(CalzyrApplication.class, args);
 	}
