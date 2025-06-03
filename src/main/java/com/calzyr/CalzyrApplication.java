202ff6ca1d617bc227738152d0fef9ed87890407
Jesse <Jessedekoe03@gmail.com>
2025-06-03 13:00:05 +0200

    removed unused imports

--- src/main/java/com/calzyr/CalzyrApplication.java
+++ src/main/java/com/calzyr/CalzyrApplication.java
@@ -1,15 +1,12 @@
 package com.calzyr;
 
 import com.calzyr.config.EnvConfig;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaRepositories(basePackages = "com.calzyr.repositories")
 public class CalzyrApplication {
 
 	public static void main(String[] args) {
