9e1865006d4edefb0d21424ddb56bf97b77490b3
Jesse <Jessedekoe03@gmail.com>
2025-06-03 13:02:06 +0200

    Added the RefreshTokenDTO

---
+++ src/main/java/com/calzyr/dto/authentication/RefreshTokenDTO.java
@@ -0,0 +1,37 @@
+package com.calzyr.dto.authentication;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "refresh_tokens")
+public class RefreshTokenDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "refresh_token_id")
+    private Integer id;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "access_token")
+    private String accessToken;
+
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    @Column(name = "issued_at")
+    private Timestamp issuedAt;
+
+    @Column(name = "refreshed_at")
+    private Timestamp refreshAt;
+
+    @Column(name = "expired")
+    private Boolean expired = false;
+}
