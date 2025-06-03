633667702fd59d4414ee14a8cad94e3239b0a133
Jesse <Jessedekoe03@gmail.com>
2025-06-03 13:03:02 +0200

    fetch token with username and or email

--- src/main/java/com/calzyr/repositories/UserRepository.java
+++ src/main/java/com/calzyr/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.calzyr.repositories;
 import com.calzyr.dto.user.UserDTO;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -15,8 +16,11 @@ public interface UserRepository extends CrudRepository<UserDTO, Integer> {
     @Query("SELECT u FROM UserDTO u WHERE u.DeletedAt IS NULL")
     Iterable<UserDTO> findAllActiveUsers();
 
-    @Query("SELECT u.Username, u.Email FROM UserDTO u WHERE u.DeletedAt IS NULL AND (u.Username = :username OR u.Email = :email)")
+    @Query("SELECT u FROM UserDTO u WHERE u.DeletedAt IS NULL AND (u.Username = :username OR u.Email = :email)")
     Optional<UserDTO> findByUsernameOrEmail(String username, String email);
+
+    @Query("SELECT u.Id FROM UserDTO u WHERE u.DeletedAt IS NULL AND (u.Username = :input OR u.Email = :input)")
+    Integer getIdByUsernameOrEmail(@Param("input") String usernameOrEmail);
 
     @Query("SELECT u FROM UserDTO u WHERE u.DeletedAt IS NOT NULL AND u.DeletedAt < :date")
     List<UserDTO> findAllInactiveUsers(LocalDateTime date);
