51a471a5a7bf14d9706c4626092fe5445de50a7f
Jesse <Jessedekoe03@gmail.com>
2025-06-17 15:51:24 +0200

    unused import

--- src/main/java/com/calzyr/services/user/UserService.java
+++ src/main/java/com/calzyr/services/user/UserService.java
@@ -8,7 +8,6 @@ import com.calzyr.exceptions.NotFoundException;
 import com.calzyr.repositories.EventRepository;
 import com.calzyr.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
