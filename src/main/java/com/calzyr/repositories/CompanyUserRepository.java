24d5cd583f2fc094e0f179485ee76911103afc2a
jesse <Jessedekoe03@gmail.com>
2025-07-18 15:26:02 +0200

    Added queries in their repositories to get the right details

--- src/main/java/com/calzyr/repositories/CompanyUserRepository.java
+++ src/main/java/com/calzyr/repositories/CompanyUserRepository.java
@@ -2,13 +2,15 @@ package com.calzyr.repositories;
 
 import com.calzyr.entities.company.CompanyUser;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CompanyUserRepository {
+public interface CompanyUserRepository extends CrudRepository<CompanyUser, Integer> {
 
     @Query("SELECT cu.UserId, cu.CompanyId FROM CompanyUser cu WHERE cu.UserId = :userId AND cu.DeletedAt IS NULL")
     Iterable<CompanyUser> findByUserId(@Param("userId") Integer userId);
@@ -18,4 +20,7 @@ public interface CompanyUserRepository {
 
     @Query("SELECT cu.UserId, cu.CompanyId FROM CompanyUser cu WHERE cu.UserId = :userId AND cu.CompanyId = :companyId AND cu.DeletedAt IS NULL")
     Optional<CompanyUser> findUserInCompany(@Param("userId") Integer userId, @Param("companyId") Integer companyId);
+
+    @Query("SELECT cu.CompanyId FROM CompanyUser cu WHERE cu.UserId = :userId AND cu.DeletedAt IS NULL")
+    List<Integer> findCompanyOfUser(Integer userId);
 }
