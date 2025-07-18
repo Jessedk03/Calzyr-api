0cc98e73f8b8bea5d24623f7d2bf5d5e447f2667
jesse <Jessedekoe03@gmail.com>
2025-07-18 15:26:42 +0200

    added queries

--- src/main/java/com/calzyr/repositories/SubscriptionRepository.java
+++ src/main/java/com/calzyr/repositories/SubscriptionRepository.java
@@ -1,16 +1,24 @@
 package com.calzyr.repositories;
 
 import com.calzyr.entities.subscription.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
-public interface SubscriptionRepository {
+public interface SubscriptionRepository extends JpaRepository<Subscription, Integer> {
 
-    @Query("SELECT s FROM Subscription WHERE s.SubscriptionId = :subscriptionId AND s.DeletedAt IS NULL")
+    @Query("SELECT s FROM Subscription s WHERE s.DeletedAt IS NULL")
+    Iterable<Subscription> findAllSubscriptions();
+
+    @Query("SELECT s FROM Subscription s WHERE s.Id IN :subscriptionIds AND s.DeletedAt IS NULL")
+    List<Subscription> findAllSubscriptionsById(@Param("subscriptionIds") List<Integer> subscriptionIds);
+
+    @Query("SELECT s FROM Subscription s WHERE s.Id = :subscriptionId AND s.DeletedAt IS NULL")
     Optional<Subscription> findById(@Param("subscriptionId") Integer subscriptionId);
 
 }
